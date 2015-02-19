33d3ff936bdbe45a766d88ace7155955b63b3e07
Artusoft <diego@artusoft.com>
2015-02-19 11:30:22 +0100

    First commit

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+public static class DateTimeExtensions
+{
+  /// <summary>
+  /// Returns true if the system's time zone is now experiencing a period of
+  /// Daylight Savings adjustment. e.g. one hour added
+  /// </summary>
+  public static bool IsDaylightSavingTime()
+  {
+    return TimeZone.CurrentTimeZone.IsDaylightSavingTime(DateTime.Now);
+  }
+}
